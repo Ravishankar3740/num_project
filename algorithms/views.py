a0ad07dd6a1451d267ac6def6a056190e0fa42bd
Ravishankar3740 <ravi.birajdar8880@gmail.com>
2021-08-22 10:47:13 +0530

    ok

--- algorithms/views.py
+++ algorithms/views.py
@@ -10,6 +10,12 @@ def addition(n):
             temp = temp + int(i)
         return addition(temp)
 
+def addition1(n):
+    temp = 0
+    for i in str(n):
+        temp = temp + int(i)
+    return temp
+    # return addition(temp)
 
 # Create your views here.
 @csrf_exempt
@@ -21,15 +27,38 @@ def take_data(request):
     print(request.POST)
     date_of_birth = request.POST.get('birth_date')
     a = date_of_birth.split('-')
+    print(a)
     strings = [str(integer) for integer in a]
     a_string = "".join(strings)
     an_integer = a_string
-    total = addition(an_integer)
-    d = addition(int(a[0]))
+    conductor = addition(an_integer)
+    driver = addition(int(a[-1]))
+
     if request.POST.get('gender')=='Male':
-        c = 11-addition(int(a[1]))
+        kua = 11-addition(int(a[0]))
+        kua = addition(kua)
     else:
-        c = 4+addition(int(a[1]))
-    k = addition(int(a[-1]))
-    dd = {"name":request.POST.get('name'),"birth_date":request.POST.get('birth_date'),"gender":request.POST.get('gender'),"total":total,"driver":d,"cund":c,"kuber":k}
+        kua = 4+addition(int(a[0]))
+        kua = addition(kua)
+
+
+    if request.POST.get('mobile1') and request.POST.get('mobile2'):
+        d = [request.POST.get('mobile1'),request.POST.get('mobile2')]
+        print(d)
+        daa = {}
+        for i in range(0,2):
+            mob_add = addition(d[i])
+            daa['mob'+str(i)] = mob_add
+            # mob_two_dig = addition1(d[i])
+            daa['mob_two'+str(i)] = addition1(d[i])
+        dd = {"name":request.POST.get('name'),"birth_date":request.POST.get('birth_date'),"gender":request.POST.get('gender'),"driver":driver,"cund":conductor,"kuber":kua,'mob1':daa['mob0'],'mob2':daa['mob1'],"num1":request.POST.get('mobile1'),"num2":request.POST.get('mobile2'),"two_mob1":daa['mob_two0'],"two_mob2":daa['mob_two1']}
+
+    else:
+        daa = {}
+        mob_add = addition(request.POST.get('mobile1'))
+        daa['mob'+str(1)] = mob_add
+        daa['mob_two'+str(1)] = addition1(request.POST.get('mobile1'))
+        dd = {"name":request.POST.get('name'),"birth_date":request.POST.get('birth_date'),"gender":request.POST.get('gender'),"driver":driver,"cund":conductor,"kuber":kua,'mob1':daa['mob1'],"num1":request.POST.get('mobile1'),"two_mob1":daa['mob_two1']}
+    print(dd)
+    
     return render(request,"algorithms/data.html",{"data":dd})
